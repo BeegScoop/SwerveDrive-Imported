935588ba1f145705cf8054636e34033e8e69edbe
Brandon Forseth <bfclashman15@gmail.com>
2024-03-01 16:07:04 -0700

    added constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -105,7 +105,7 @@ public final class Constants {
     public static final double kMeterLimitBot = 0;
     public static final double kMeterLimitTop = 0;
     //check
-    public static final int kWinchMotorPort = 106;
+    public static final int kWinchMotorPort = 40;
     public static final boolean kWinchMotorReversed = false;
     public static final double kWinchForwardSpeed =1.0;
     public static final double kWinchBackwardSpeed = -1.0;
